81a16031cc00677376ebb0783f7d862dffede37b
Kostin Dmitrij <kostindima@gmail.com>
2010-11-19 16:29:55 +0300

    123

--- page.php
+++ page.php
@@ -1,63 +1,175 @@
 <?php
 
-function printArray( $a ){
+$db = new SQLite3('/srv/www/htdocs/sites/all/modules/okved/qokved.db', 0666, $error);
+$version = 1;
+
+function form_filter_submit($form, &$form_state) {
+  $form_state['redirect'] = 'okved/search/' . $form_state['values']['filter'];
+}
+
+function form_filter($form_state) {
+  $form['filter'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Поиск'),
+    '#required' => TRUE,
+    '#size' => 40,
+    '#maxlength' => 40,
+  );
+  $form['submit'] = array(
+    '#type' => 'submit',
+    '#value' => 'Найти',
+  );
+  return $form;
+}
+
+function razdels_print()
+{
+$db = new SQLite3('/srv/www/htdocs/sites/all/modules/okved/qokved.db', 0666, $error);
+$version = 1;
+
+$q = $db->query('SELECT * FROM razdelz_'.$version);
+  	
+	$table_attributes = array('id' => 'rasdels_list');
+	$headers = array('Раздел');
+	while ( ($row = $q->fetchArray()))
+	{
+		$razdel_link = url('okved/rasdel/'.$row['rid'], array('absolute' => TRUE));
+		$rows[] = array(l($row['name'], $razdel_link));
+	}
+//+ theme('table', $headers, $rows, $table_attributes)
+return drupal_get_form('form_filter') . theme('table', $headers, $rows, $table_attributes) ;	
+
+}
+
+function okveds_from_query($q)
+{
+	drupal_add_js('$(document).ready(function(){
+ 
+    $("#okveds_list .block").hover(function(){
+		$(this).find("p").slideToggle("fast");
+     }, function() {
+		$(this).find("p").slideToggle("fast");
+	 });});', 'inline');
+	
+	$table_attributes = array('id' => 'okveds_list');
+	$headers = array('Номер', 'Наименование / Дополнительное описание при наведении');
+	while ( ($row = $q->fetchArray()))
+	{
+		if ($row['addition'] != "") 
+		{
+			$rows[] = array('class' => 'block',
+							'data' => array(array('data' => $row['number'], 'valign' => 'top'), 
+							$row['name'].sprintf('<p style="display: none;">%s</p>', $row['addition'])));
+		} else {
+			
+			$rows[] = array(array('data' => $row['number'], 'valign' => 'top'), 
+							$row['name']);
+		}
+	}
+
+return(theme('table', $headers, $rows, $table_attributes));
+}
+
+function okveds_print($rasdel)
+{
+$db = new SQLite3('/srv/www/htdocs/sites/all/modules/okved/qokved.db', 0666, $error);
+$version = 1;
+
+$return="";	
+$filter="";
+if ($rasdel==1) {		//Если это "Все разделы"
+	$filter="";
+} else { 				//Если нет, ищем по разделу + подразделам
+	$filter=' WHERE razdel_id='.$rasdel;
+	
+	$q = $db->query('SELECT rid FROM razdelz_'.$version.' WHERE father='.$rasdel);
+	while ($row = $q->fetchArray())
+	{
+		$filter .= ' OR razdel_id='.$row['rid'];
+	}
+}
+$q = $db->query('SELECT * FROM okveds_'.$version . $filter);
   
-   static $count; 
-  $count = (isset($count)) ? ++$count : 0;
-  $colors = array('#FFCB72', '#FFB072', '#FFE972', '#F1FF72', '#92FF69', '#6EF6DA', '#72D9FE', '#77FFFF', '#FF77FF');
-  if ($count > count($colors)) {
-   $count--;
-   return;
-  }
+return drupal_get_form('form_filter') . okveds_from_query($q);
+}
 
-  if (!is_array($a)) {
-   echo "Passed argument is not an array!<p>";
-   return; 
-  }
+function rasdel_name($rasdel)
+{
+$db = new SQLite3('/srv/www/htdocs/sites/all/modules/okved/qokved.db', 0666, $error);
+$version = 1;
 
-  echo "<table border=1 cellpadding=0 cellspacing=0 bgcolor=$colors[$count]>";
+$q = $db->query('SELECT * FROM razdelz_'.$version . ' WHERE rid='.$rasdel . ' LIMIT 1');
+$row = $q->fetchArray();
 
-  while(list($k, $v) = each($a)) {
-   echo "<tr><td style='padding:1em'>$k</td><td style='padding:1em'>$v</td></tr>\n";
-   if (is_array($v)) {
-    echo "<tr><td> </td><td>";
-    self::printArray($v);
-    echo "</td></tr>\n";
-   }
-  }
-  echo "</table>";
-  $count--;
- }
+return $row['name'];
+}
 
 
-$error="";
-    if ($db = new SQLite3('qokved.db', 0666, $error)) {
-		
-        $q = $db->query('SELECT * FROM versions');
-  
-        echo $q->numColumns();
-
+class Okved {
+	protected $version = 1;
+	protected $db;
+	public function __construct( /*...*/ ) {
+		$error="";	
+		$this->db = new SQLite3('/srv/www/htdocs/sites/all/modules/okved/qokved.db', 0666, $error);
+    }
+    
+	public function print_okved()
+	{
+	$return="";		
+	$q = $this->db->query('SELECT * FROM versions');
+	  
+	$return .= '<form>
+			<select name="ComboBox" style="width : 200">';
+		  
 		while ( ($row = $q->fetchArray()))
 		{
-				printf("%b %-20s\n", $row['id'], $row['name']);
+			$return .= sprintf( "<option value=\"%b\">%s</option>", $row['id'], $row['name']);
 		}
 
-        print_r ($row);
-		//printArray($row);
+	$return .=       '</select>
+	</form>';
 
-     /*   if ($q) {
-            $db->queryExec('CREATE TABLE tablename (id int, requests int, PRIMARY KEY (id)); INSERT INTO tablename VALUES (1,1)');
-            $hits = 1;
-        } else {
-            $result = $q->fetchSingle();
-            $hits = $result+1;
-        }
-        $db->queryExec("UPDATE tablename SET requests = '$hits' WHERE id = 1");
-       */ 
-    } else {
-		//output=$error;
-        die($error);
-      
-    }
+	return($return);
+	}
+	
+	function form_example_tutorial_2(&$form_state) {
+  $form['description'] = array(
+    '#type' => 'item',
+    '#title' => t('A simple form with a submit button'),
+  );
+
+  $form['name'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Name'),
+  );
+
+  // Adds a simple submit button that refreshes the form and clears its contents -- this is the default behavior for forms.
+  $form['submit'] = array(
+    '#type' => 'submit',
+    '#value' => 'Submit',
+  );
+  return $form;
+}
+
+	public function okveds_array()
+	{
+	$return="";		
+	$q = $this->db->query('SELECT * FROM razdelz_'.$version);
+	  
+	$return .= '<form>
+			<select name="ComboBox" style="width : 200">';
+		  
+		while ( ($row = $q->fetchArray()))
+		{
+			$return .= sprintf( "<option value=\"%b\">%s</option>", $row['id'], $row['name']);
+		}
+
+	$return .=       '</select>
+	</form>';
+
+	return($return);
+	}
+
+}
 
 ?>
